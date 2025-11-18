d42b26be580fd10411cfcaeab5ef8088f28d56f2
pandson7 <pandson@amazon.com>
2025-11-18 12:15:38 -0500

    Add getProducts Lambda function

---
+++ lambda/getProducts.js
@@ -0,0 +1,36 @@
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient, ScanCommand } = require('@aws-sdk/lib-dynamodb');
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+exports.handler = async (event) => {
+    try {
+        const command = new ScanCommand({
+            TableName: process.env.TABLE_NAME
+        });
+        
+        const result = await docClient.send(command);
+        
+        return {
+            statusCode: 200,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type'
+            },
+            body: JSON.stringify(result.Items || [])
+        };
+    } catch (error) {
+        console.error('Error:', error);
+        return {
+            statusCode: 500,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({ error: 'Internal server error' })
+        };
+    }
+};
